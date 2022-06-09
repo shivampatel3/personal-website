be65be4ff358c28981fe76156512b9e4f9a2b789
Shivam <shivampatel2468@gmail.com>
2022-06-08 21:40:54 -0400

    verion 1.0 of website

---
+++ src/data.js
@@ -0,0 +1,108 @@
+module.exports = {
+    "socials": [
+        {
+            "icon": "twitter",
+            "url": "http://twitter/"
+        },
+        {
+            "icon": "github-circled",
+            "url": "http://github.com/shivampatel3"
+        },
+        {
+            "icon": "dribbble",
+            "url": ""
+        },
+        {
+            "icon": "skype",
+            "url": ""
+        },
+        {
+            "icon": "mail-alt",
+            "url": "mailto: email"
+        },
+        {
+            "icon": "linkedin-squared",
+            "url": "https://www.linkedin.com/in/shivam-patel-663401152/"
+        },
+        {
+            "icon": "icon-mail-alt",
+            "url": "shivam.patel@gatech.edu"
+        }
+    ],
+    "experiences": [
+        {
+            "__key": "a9e38603-4068-4470-9589-619d7ac48e57",
+            "title": "Software Engineering Intern @ NCR Corporation",
+            "timeline": "May 2022 - Present",
+            "description": "Currently working with the Innovation Lab and Aloha Cloud teams on a variety of projects to hone my skills in full stack and machine learning engineering. "
+        },
+        {
+            "__key": "f9786ed9-7a74-41c0-a04f-84691ccb71b9",
+            "title": "Undergraduate Researcher @ Mitchell Lab",
+            "timeline": "Jan 2022 - Present",
+            "responsibilities": [],
+            "description": "This has been one of my first experiences with machine learning and AI. It has been wonderful to combine two of my interests while working on research which could have enormous value throughout the biomedical field. "
+        },
+        {
+            "__key": "9636e01b-341d-4670-99f2-f48cfcc5b665",
+            "title": "Medical Scribe @ Piedmont Atlanta Hospital",
+            "timeline": "Jun 2021 - Jan 2022",
+            "description": "I worked as a medical scribe in the Emergency Department at Piedmont Atlanta Hospital. Up till this point I had juggled interests between medicine and technology but this experience led me to realize that technology was the field I wanted to pursue. I decided to pull the trigger and switch my major to computer science. "
+        },
+        {
+            "__key": "75341d9f-035c-4981-a933-e2b461364761",
+            "title": "Peer Led Undergraduate Studies @ Georgia Tech (PLUS) Leader",
+            "timeline": "Jan 2021 - May 2021",
+            "description": "I was essentially a tutor for a first year chemistry class. I led biweekly review sessions for the class and enjoyed helping the students succeed and build good study habits. ",
+            "responsibilities": []
+        },
+        {
+            "__key": "ec0b649c-da9e-440e-9391-6456ef4aad34",
+            "title": "Undergraduate Researcher @ Coskun Lab",
+            "timeline": "Nov 2019 - Aug 2021",
+            "description": "My first experience with research as a college student. I started with wet lab but quickly realized that it wasn't for me. I moved to a more computational role in the lab and begin developing a passion for solving problems with technology. "
+        },
+        {
+            "__key": "68bf99c7-ceba-498f-93b3-c55614d56693",
+            "title": "Research Intern @ Chemlink Labs",
+            "timeline": "May 2022 - Present",
+            "description": "I worked on a project to quantify the amount of active enzyme in one of the company's major products. I didn't realize it at the time, but computing was at the heart of what I was doing. "
+        }
+    ],
+    "name": "Shivam Patel",
+    "header": "Hi, I'm Shivam Patel",
+    "cta": {
+        "label": "Get my resume",
+        "url": "resume.pdf"
+    },
+    "testimonials": [],
+    "title": "Shivam Patel",
+    "projects": [
+        {
+            "__key": "d300ab74-ae3b-43c5-bd61-0cc31ac7a589",
+            "name": "2048",
+            "description": "Working on a clone of the game 2048 to practice my front end web development basics. Maybe I won't be so bad at the game after building it...",
+            "tags": [
+                "HTML",
+                "CSS",
+                "Javascript"
+            ],
+            "alt": "2048 Screenshot",
+            "img": "2048.png",
+            "url": "//"
+        },
+        {
+            "__key": "89f0ae37-86de-494b-92de-a8f251c8a8e7",
+            "name": "Stress Monitor Project",
+            "description": "I worked on a group project to build a non invasive device that could monitor stress. I worked mainly on the software implementation for analyzing the data received by the heart rate monitor. ",
+            "tags": [
+                "Python"
+            ],
+            "img": "stress-monitor.png",
+            "url": "//"
+        }
+    ],
+    "email": "shivam.patel@gatech.edu",
+    "description": "I'm a fourth year computer science student at the Georgia Institute of Technology. I'm deeply curious and enjoy learning and applying machine learning, full-stack web development, and quantitative modeling wherever I can provide value. In my free time I enjoy watching soccer, playing basketball, and playing card and board games. ",
+    "footerTitle": "Student | Software Engineer"
+}
